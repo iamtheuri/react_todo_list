86560711d86042f7b298592968f002511d2b2ad8
iamtheuri <theuridavid56@gmail.com>
2024-01-02 12:21:25 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+
+export default function App() {
+  return <div>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+  </div>
+}
